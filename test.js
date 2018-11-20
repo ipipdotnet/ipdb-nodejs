4f64f7627244511d1310627f897b1b8cfebd9d4e
ipipdotnet <frk@ipip.net>
2018-11-20 22:40:20 +0800

    update

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
-var Reader = require('./lib/reader');
+/*
 
-db = new Reader("c:/work/ipdb/city.free.ipdb");
-
-console.log(db.find("1.1.1.1", "CN"));
 
 var city = require('./lib/city');
 
@@ -12,3 +9,15 @@ var info = c.findInfo("118.28.1.1", "CN");
 
 console.log(info.cityName);
 console.log(info);
+
+
+var Reader = require('./lib/reader');
+
+db = new Reader("c:/work/ipdb/city.free.ipdb");
+
+console.log(db.find("1.1.1.1", "CN"));
+*/
+var BaseStation = require('./lib/base_station')
+
+var bst = new BaseStation('c:/tiantexin/download/base_station.ipdb');
+console.log(bst.findInfo("223.220.231.255", "CN"));
