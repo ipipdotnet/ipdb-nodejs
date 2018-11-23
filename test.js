8e8f1f9b3655c4ba922c83a4c9efffd82d30bb14
小宝 <ilsanboo@gmail.com>
2018-11-23 14:19:05 +0800

    update

--- test.js
+++ test.js
@@ -1,23 +1,16 @@
-/*
 
+var ipdb = require('./index')
 
-var city = require('./lib/city');
+function testBaseStation() {
+    var bst = new ipdb.BaseStation('c:/tiantexin/download/base_station.ipdb');
+    console.log(bst.findInfo("223.220.231.255", "CN"));
+}
 
-var c = new city("c:/work/ipdb/city.free.ipdb");
+testBaseStation();
 
-var info = c.findInfo("118.28.1.1", "CN");
+function testCity() {
+    var city = new ipdb.City('c:/work/ipdb/city.free.ipdb');
+    console.log(city.findInfo("118.28.1.1", "CN"));
+}
 
-console.log(info.cityName);
-console.log(info);
-
-
-var Reader = require('./lib/reader');
-
-db = new Reader("c:/work/ipdb/city.free.ipdb");
-
-console.log(db.find("1.1.1.1", "CN"));
-*/
-var BaseStation = require('./lib/base_station')
-
-var bst = new BaseStation('c:/tiantexin/download/base_station.ipdb');
-console.log(bst.findInfo("223.220.231.255", "CN"));
+testCity();
